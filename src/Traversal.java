13e3c483621fee980fb91242a477b076e0ac0c11
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2026-02-17 13:20:15 -0800

    generics

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,13 +1,13 @@
 public class Traversal {
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(10, null, null);
-    root.left = new TreeNode(9, null, null);
-    root.left.left = new TreeNode(5, null, null);
-    root.left.right = new TreeNode(2, null, null);
-    root.right = new TreeNode(15, null, null);
-    root.right.left = new TreeNode(-3, null, null);
-    root.right.right = new TreeNode(5, null, null);
-    root.right.right.right = new TreeNode(22, null, null);
+    TreeNode<Integer> root = new TreeNode<Integer>(10, null, null);
+    root.left = new TreeNode<Integer>(9, null, null);
+    root.left.left = new TreeNode<Integer>(5, null, null);
+    root.left.right = new TreeNode<Integer>(2, null, null);
+    root.right = new TreeNode<Integer>(15, null, null);
+    root.right.left = new TreeNode<Integer>(-3, null, null);
+    root.right.right = new TreeNode<Integer>(5, null, null);
+    root.right.right.right = new TreeNode<Integer>(22, null, null);
 
     System.out.println("======PRE ORDER======");
     preorder(root);
@@ -18,21 +18,21 @@ public class Traversal {
   }
 
     //print tree rooted at given node in pre-order.
-  public static void preorder(TreeNode node) {
+  public static void preorder(TreeNode<?> node) {
     if(node == null) return;
     System.out.println(node.value);
     preorder(node.left);
     preorder(node.right);
   }
 
-  public static void postorder(TreeNode node) {
+  public static void postorder(TreeNode<?> node) {
     if(node == null) return;
     postorder(node.left);
     postorder(node.right);
     System.out.println(node.value);
   }
 
-  public static void inorder(TreeNode node) {
+  public static void inorder(TreeNode<?> node) {
     if(node == null) return;
     inorder(node.left);
     System.out.println(node.value);
