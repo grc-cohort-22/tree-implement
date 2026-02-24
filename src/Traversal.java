ebe88341cb964effa1ceedfc2f0ebb768811aaa7
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-02-24 14:53:38 -0800

    levelOrder iterative and preOrder

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,3 +1,9 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Traversal {
   public static void main(String[] args) {
     TreeNode<Integer> root = new TreeNode <Integer> (10, null, null);
@@ -13,22 +19,113 @@ public class Traversal {
 
     TreeNode <String> stringRoot = new TreeNode<String> ("Hello", null, null);
     stringRoot.left = new TreeNode <String> ("how", null,null);
-    stringRoot.left.left = new TreeNode<String> ("are", null,null);
+    stringRoot.left.left = new TreeNode<String> ("Hello", null,null);
     stringRoot.left.right = new TreeNode <String> ("you", null,null);
 
     stringRoot.right = new TreeNode <String> ("I", null, null);
     stringRoot.right.left = new TreeNode <String> ("am", null, null);
-    stringRoot.right.right = new TreeNode <String> ("so", null, null);
+    stringRoot.right.right = new TreeNode <String> ("am", null, null);
     stringRoot.right.right.right = new TreeNode <String> ("good", null, null);
 
+    TreeNode<Integer> megaRoot = new TreeNode<Integer>(1, null, null);
+
+    TreeNode<Integer> current = megaRoot;
+
+    for(int i =2; i <= 5000; i++){
+      current.right = new TreeNode<Integer>(i, null, null);
+      current = current.right;
+    }
+
+    levelOrder(root);
+    //preOrderIter(megaRoot);
+
+    // System.out.println("Preorder recursive");
+    // preorder(root);
+    // System.out.println("Preorder Iterativr");
+    // preOrderIter(root);
 
     //preorder(root);
     //postorder(root);
     //inorder(root);
 
     //printGreater(root, 4);
-    System.out.println(countNodes(root));
+    //System.out.println(countNodes(root));
+    
+    //System.out.println(toMap(stringRoot));
 
+  }
+
+    // Print a tree rooted at the given node in pre-order
+  public static void preorder(TreeNode<?> node){
+    //if null, return
+    if(node == null) return;
+    //print value
+    System.out.println(node.value);
+    //traverse left to recursion and tree works
+    preorder(node.left);
+    //traverse right
+    preorder(node.right);
+
+  }
+
+  public static <T> void preOrderIter(TreeNode<T> node){
+    Stack<TreeNode<T>> stack = new Stack<>();
+
+    stack.push(node);
+
+    while(!stack.empty()){
+      TreeNode<T> current = stack.pop();
+
+      if(current == null){
+        continue;
+      }
+        System.out.println(current.value);
+        stack.push(current.right);
+        stack.push(current.left);
+     
+      
+      
+    }
+  }
+
+   public static <T> void levelOrder(TreeNode<T> node){
+    Queue<TreeNode<T>> queue = new LinkedList<>();
+
+    queue.add(node);
+
+    while(!queue.isEmpty()){
+      TreeNode<T> current = queue.poll();
+
+      if(current == null){
+        continue;
+      }
+        System.out.println(current.value);
+        queue.add(current.left);
+        queue.add(current.right);
+  
+      
+    }
+  }
+
+
+  public static <T> Map<T, Integer> toMap(TreeNode<T> node) {
+    Map<T, Integer> counts = new HashMap<>();
+    toMap(node, counts);
+    return counts;
+
+  }
+  //overloading and helper method
+
+  private static <T> void  toMap(TreeNode<T> node, Map<T, Integer> counts ) {
+    
+    if(node == null) {
+      return;
+    }
+    //fill up those counts
+    counts.put(node.value, counts.getOrDefault(node.value, 0) +1);
+    toMap(node.left, counts);
+    toMap(node.right, counts);
+    
 
   }
 
@@ -53,18 +150,7 @@ public class Traversal {
  }
 
 
-  // Print a tree rooted at the given node in pre-order
-  public static void preorder(TreeNode<?> node){
-    //if null, return
-    if(node == null) return;
-    //print value
-    System.out.println(node.value);
-    //traverse left to recursion and tree works
-    preorder(node.left);
-    //traverse right
-    preorder(node.right);
 
-  }
 
   public static <T> void postorder(TreeNode<T> node){
     if(node == null) return;
